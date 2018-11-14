158c3da8bea7d0fd57260f6bf5cbf42490de2c69
Vladislav Bogdanov <36765224+vlhell@users.noreply.github.com>
2018-11-14 15:16:26 +0300

    added files via upload

---
+++ hello.c
@@ -0,0 +1,24 @@
+/* hello.c */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+    pr_alert("twas init and HELLO!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_alert("twas exit and GOODBYE!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRPTION("Hello-world or greeting module");
+MODULE_AUTHOR("Vladislav Bogdanov");
+
+/* you are my reggae untouchable */
